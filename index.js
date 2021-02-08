e13202e58a82e2f1c456035adfd578b06c150de6
Maxence Derepas <maxence.derepas@gmail.com>
2021-02-08 20:25:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const data = require("./data.json");
+
+app.get("/", (req, res) => {
+    res.json(data);
+});
+
+app.listen(3100, () => {
+    console.log("server started");
+});
